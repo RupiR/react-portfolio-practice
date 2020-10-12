b9a10fdd4b3c146ae2548be33718f6dc53009c17
RupiR <rupalirajput@gmail.com>
2020-10-12 12:22:09 -0500

    added megatron to hero

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+function Hero() {
+
+return(
+    <Jumbotron></Jumbotron>
+);
+
+}
+
+export default Hero;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Hero from '../components/Hero';
+
 function HomePage(props) {
 
     return(
-        <p>Home Page works!</p>
+        <Hero />
     );
 
 }
